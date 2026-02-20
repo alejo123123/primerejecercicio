9bc0f034cdac67c1d79382d8d4a767880108772d
alejo123123 <alejobustamanter@gmail.com>
2026-02-20 17:21:57 -0500

    nueva clase

--- src/clase.java
+++ src/clase.java
@@ -1,3 +1,29 @@
-public class clase {
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.table.DefaultTableModel;
+
+
+
+public class Clase extends JFrame{
+
+
+
+    public Clase(){
+
+        setTitle("Distribución de frecuencias");
+        setSize(500, 500);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setLayout(null);
+        
+    }
 
 }

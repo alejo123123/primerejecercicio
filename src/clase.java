de1d714d712f8139b391a07227b71a3f2e26d453
alejo123123 <alejobustamanter@gmail.com>
2026-02-20 20:23:04 -0500

    hice que funcionara el primer boton de agregar billetes, le agregue un evento

--- src/clase.java
+++ src/clase.java
@@ -5,11 +5,14 @@ public class Clase extends JFrame {
 
     String[] opciones = { "50000", "20000", "10000", "5000", "2000", "1000", "500", "200", "100", "50" };
     String[] encabe = { "cantidad", "presentacion", "denominacion" };
+    String[] presentaciones = { "billete", "billete", "billete", "billete", "billete", "moneda", "moneda", "moneda", "moneda", "moneda" };
     int[] existencias = {0,0,0,0,0,0,0,0,0,0};
     int [] devuelta = {0,0,0,0,0,0,0,0,0,0};
     private JTextField txtCantidad, txtexi;
     private JComboBox cmbRespuesta;
     JTable tblplata;
+   
+    
 
     public Clase() {
 
@@ -60,6 +63,35 @@ public class Clase extends JFrame {
 
         DefaultTableModel modelo = new DefaultTableModel(null, encabe);
         tblplata.setModel(modelo);
+
+
+        btnAgregar.addActionListener(e -> {
+            agregare();
+        });
     }
 
+    private void agregare() {
+        int index = cmbRespuesta.getSelectedIndex();
+        String num = txtexi.getText();
+        existencias[index] = Integer.parseInt(num);
+        txtexi.setText("");
+        DefaultTableModel modelo = (DefaultTableModel) tblplata.getModel();
+        modelo.setRowCount(0);
+      
+
+        for (int i = 0; i<opciones.length;i++) {
+
+        Object[] fila = { 
+            existencias[i], 
+            presentaciones[i], 
+            opciones[i] 
+        };
+        modelo.addRow(fila);
+
+        
+         
+    }
+
+        
+    }
 }
